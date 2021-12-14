36407cb103cfa2fe86f9a99bc33cfc4954a372d6
purvangdada <pdada2@illinois.edu>
2021-12-13 20:17:49 -0600

    BFT test case

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,10 +2,27 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 
+TEST_CASE("BFT works") {
+    FlightGraph f("graph/airports.dat", "graph/routes.dat");
+    f.initialize();
+    auto result = f.BFT(0);
+    for(unsigned i = 0; i < result.size(); i++) {
+        cout << result[i];
+        /*if (result[i] == result[i++]) {
 
-TEST_CASE("B") {
-    FlightGraph f;
-    f.pars
+            return false;
+        }*/
 
+        // each airport only shows up once
+        // every airport is traversed
+        // create a set [airport]
+        check if the airport is already in the set
+            return false
+        else
+            add the current to the set
+
+    }
+    return true;
 }
