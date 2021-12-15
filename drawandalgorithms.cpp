6690874c3dbadaa0af6d1e1e0f441684b6371c94
Teague Mitchell <thm2@illinois.edu>
2021-12-15 13:46:37 -0600

    made smallgraph

--- drawandalgorithms.cpp
+++ drawandalgorithms.cpp
@@ -52,7 +52,7 @@ int main() {
             colnum = nodecol[it1 -> first];
     }
     std::cout << "Reached line " << __LINE__ << std::endl;
-  //  std::unordered_map<int, int> nodesize = betweennesscentrality(graph);
+    //std::unordered_map<int, int> nodesize = betweennesscentrality(graph);
 
     std::unordered_map<int, std::vector<Edge>>::iterator it2;
     for (it2 = graph.edges.begin(); it2 != graph.edges.end(); it2++) {
@@ -61,8 +61,8 @@ int main() {
             int end =   it->getDestination();
             int startx = int(10*((graph.airports[start].getLongitude()) + 180) + 20);
             int starty = int(10*((graph.airports[start].getLatitude()) + 90) + 10);
-            int endx = int(10*((graph.airports[end].getLongitude()) + 180));
-            int endy = int(10*((graph.airports[end].getLatitude()) + 90));
+            int endx = int(10*((graph.airports[end].getLongitude()) + 180) + 20);
+            int endy = int(10*((graph.airports[end].getLatitude()) + 90) + 10);
             Drawable* line = new Line(Vector2(startx, starty), Vector2(endx, endy), linecolor);
             lines.push_back(line);
             std::cout << "running betweeness" << std::endl;
@@ -73,7 +73,7 @@ int main() {
     for (it3 = graph.airports.begin(); it3 != graph.airports.end(); it3++) {
         int centerx = int(10*((it3 -> second.getLongitude()) + 180) + 20);
         int centery = int(10*((it3 -> second.getLatitude()) + 90) + 10);
-        int radius = 5;//*nodesize[it3 -> first];
+        int radius = 5;//*(nodesize[it3 -> first] + 1);
         double huediff = 360 / (colnum + 1);
         int fac = (nodecol[it3 -> first]/2) - 1;
         double hue;
