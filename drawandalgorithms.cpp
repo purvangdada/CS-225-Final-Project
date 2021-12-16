4971ed5606feac3bc18f07082e3feaa099ae2fdc
Teague Mitchell <thm2@illinois.edu>
2021-12-15 19:14:49 -0600

    last touches, comments

--- drawandalgorithms.cpp
+++ drawandalgorithms.cpp
@@ -32,17 +32,20 @@
 using cs225::PNG;
 using cs225::HSLAPixel;
 using std::vector;
-
+//this function draws a graph
 int main() {
     std::cout << "Reached line " << __LINE__ << std::endl;
-    FlightGraph graph("graph/truncairports.dat", "graph/routes.dat");
+    // create graph and canvas
+    FlightGraph graph("graph/trairports.dat", "graph/routes.dat");
     PNG canvas;
+    // routes will be lines, airports will be circles
     std::vector<Drawable*> lines;
     std::vector<Drawable*> circles;
     std::cout << "Reached line " << __LINE__ << std::endl;
     HSLAPixel linecolor(0, 1, 0);
     canvas.resize(4000, 2000);
     std::cout << "Reached line " << __LINE__ << std::endl;
+    // graph coloring is used to color the circles
     std::unordered_map<int, int> nodecol = graphcoloring(graph);
     int colnum = 1;
 
@@ -52,9 +55,11 @@ int main() {
             colnum = nodecol[it1 -> first];
     }
     std::cout << "Reached line " << __LINE__ << std::endl;
+    // betweenness centrality is used to determine node size
     std::unordered_map<int, int> nodesize = betweennesscentrality(graph);
 
     std::unordered_map<int, std::vector<Edge>>::iterator it2;
+    // for each edge, create a line with a start and endpoints determined by longitude and latitude
     for (it2 = graph.edges.begin(); it2 != graph.edges.end(); it2++) {
         for (auto it = (it2 -> second).begin(); it != (it2 -> second).end(); it++) {    
             int start = it->getSource();
@@ -70,13 +75,16 @@ int main() {
     }
     std::cout << "Reached line " << __LINE__ << std::endl;
     std::unordered_map<int, Node>::iterator it3;
+    // for each airport, create a circle with size determined by betweenness centrality and color determined by graph coloring
     for (it3 = graph.airports.begin(); it3 != graph.airports.end(); it3++) {
         int centerx = int(10*((it3 -> second.getLongitude()) + 180) + 200);
         int centery = int(10*((it3 -> second.getLatitude()) + 90) + 100);
         int radius = int(2*((nodesize[it3 -> first])/1000 + 1));
+        // separate colors by as much as possible
         double huediff = 360 / (colnum + 1);
         int fac = (nodecol[it3 -> first]/2) - 1;
         double hue;
+        // adjacent colors should be as different as possible for legibility
         if (nodecol[it3 -> first] % 2 == 0)
             hue = 180 + fac*huediff;
         else
@@ -85,6 +93,7 @@ int main() {
         circles.push_back(circle);
     }
     std::cout << "Reached line " << __LINE__ << std::endl;
+    // draw and then delete all lines and circles
     while (!lines.empty()) {
         std::cout << "Reached line " << __LINE__ << std::endl;
         lines.back()->draw(&canvas);
@@ -97,6 +106,7 @@ int main() {
         delete circles.back();
         circles.pop_back();
     }  
+    //write canvas to image
     std::cout << "Reached line " << __LINE__ << std::endl;
     canvas.writeToFile("graph_drawing.png");
     std::cout << "Reached line " << __LINE__ << std::endl;
